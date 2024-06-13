242e3e01c01214debabb16aa42ba69572d86562a
JIAYUE MA <oldhorse1984@gmail.com>
2024-06-12 23:34:10 -0400

    temp up

--- js/main.js
+++ js/main.js
@@ -14,3 +14,20 @@ document.getElementById('comment-form').addEventListener('submit', function(even
 
     document.getElementById('comment-form').reset();
 });
+
+document.getElementById('comment-form-2').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username-2').value;
+    const comment = document.getElementById('comment-2').value;
+
+    const commentList = document.getElementById('comment-list-2');
+
+    const commentElement = document.createElement('div');
+    commentElement.classList.add('comment');
+    commentElement.innerHTML = `<strong>${username}</strong>: <p>${comment}</p>`;
+
+    commentList.appendChild(commentElement);
+
+    document.getElementById('comment-form-2').reset();
+});
